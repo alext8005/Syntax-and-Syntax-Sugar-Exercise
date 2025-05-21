842845218c5612b43bd7b7a96e6d48e35b794433
AlexTrejo <alext8005@yahoo.com>
2025-05-20 22:00:44 -0600

    replace with var and changed to string interpolation, used ternary

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           // Inferred Typing, replace int for var
+            var answer = 4;
+            string response;
+            // replace Concatenation with string interpolation
+            if (answer < 9) 
+            {
+                response = $"{answer} is less than nine";
+            }
+            else
+            {
+                response = $"{answer} is greater than or equal to nine";
+            }
+            //changed to ternary operator
+            Console.WriteLine(answer < 9 ? " is less than nine" : " is greater than or equal to nine");
+            
+            
         }
     }
 }
